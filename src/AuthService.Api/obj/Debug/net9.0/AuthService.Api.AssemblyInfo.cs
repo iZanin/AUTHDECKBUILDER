a55002254541ac5c2c4429f482a0940531514987
izanin <isael.zanin@outlook.com>
2025-10-17 19:01:49 -0300

    CHORE adicionando swagger/programan. cs minima

--- src/AuthService.Api/obj/Debug/net9.0/AuthService.Api.AssemblyInfo.cs
+++ src/AuthService.Api/obj/Debug/net9.0/AuthService.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f5b6b82fcabd6220efe9f3a8678482c6369d7e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
